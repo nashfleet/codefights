1592dfbd44820e3c6e8ebd21028d0d3a1ce091fb
nashfleet <nashf170@gmail.com>
2017-05-03 09:57:43 -0500

    Optimized comfortable numbers

--- arcade/the_core/lab_of_nested_loops/comfortableNumbers.py
+++ arcade/the_core/lab_of_nested_loops/comfortableNumbers.py
@@ -1,23 +1,23 @@
 """
-Let's say that number a feels comfortable with number b if a ≠ b and b lies in the segment [a - s(a), a + s(a)], where s(x) is the sum of x's digits.
+Let's say that number a feels comfortable with number b if a ≠ b and
+    b lies in the segment [a - s(a), a + s(a)], where s(x) is the
+    sum of x's digits.
 
-How many pairs (a, b) are there, such that a < b, both a and b lie on the segment [L, R], and each number feels comfortable with the other?
+How many pairs (a, b) are there, such that a < b, both a and b lie
+    on the segment [L, R], and each number feels comfortable with
+    the other?
 """
 
 
 def comfortableNumbers(L, R):
-    a = []
     e = {}
+    count = 0
 
     for z in range(L, R + 1):
         e[z] = sum(int(l) for l in str(z))
 
     for i in range(L, R + 1):
-        for j in range(i, R + 1):
-            if i == j:
-                continue
-            if ((j - e[j]) <= i <= (j + e[j])) and ((i - e[i]) <= j <= (i + e[i])):
-                d = sorted([i, j])
-                if d not in a:
-                    a.append(d)
-    return len(a)
+        for j in range(i + 1, R + 1):
+            if (j - e[j] <= i <= j + e[j]) and (i - e[i] <= j <= i + e[i]):
+                count += 1
+    return count
